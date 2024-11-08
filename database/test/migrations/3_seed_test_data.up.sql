4cecb2bc693d96385402577754f6f680cd1c9084
KnoblauchPilze <prolific-programmer@tuta.com>
2024-11-08 22:37:50 +0100

    feat: Added QueryOne and QueryAll functions to get data from the database and tests

---
+++ database/test/migrations/3_seed_test_data.up.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO test_db_schema.my_table ("id", "name")
+  VALUES 
+    ('0463ed3d-bfc9-4c10-b6ee-c223bbca0fab', 'test-name'),
+    ('09dd5fc3-0732-4017-81e0-ffee3211d2b9', 'other-name');
--- pkg/db/errors.go
+++ pkg/db/errors.go
@@ -3,5 +3,11 @@ package db
 import "github.com/KnoblauchPilze/user-service/pkg/errors"
 
 const (
-	NotConnected errors.ErrorCode = 100
+	NotConnected         errors.ErrorCode = 100
+	UnsupportedOperation errors.ErrorCode = 101
+
+	NoMatchingRows      errors.ErrorCode = 110
+	TooManyMatchingRows errors.ErrorCode = 111
+	QueryOneFailure     errors.ErrorCode = 112
+	QueryAllFailure     errors.ErrorCode = 113
 )

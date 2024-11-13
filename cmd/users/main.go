1750308a77fb01c3716022f6516b9aeec0ff12f1
KnoblauchPilze <prolific-programmer@tuta.com>
2024-11-13 22:47:45 +0100

    feat: Added server package

--- cmd/users/main.go
+++ cmd/users/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/KnoblauchPilze/user-service/pkg/server"
+)
 
 func main() {
-	fmt.Println("Hello user!")
+	config := server.Config{
+		Port:            1234,
+		ShutdownTimeout: 2 * time.Second,
+	}
+
+	s := server.New(config)
+	err := s.Start(context.Background())
+	fmt.Printf("err: %v\n", err)
 }

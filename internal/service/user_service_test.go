4ebdd3b8774ba6205375c7ca88020591eab8e139
KnoblauchPilze <prolific-programmer@tuta.com>
2024-11-15 16:01:21 +0100

    feat: Added tests for uncovered methods

--- internal/service/user_service_test.go
+++ internal/service/user_service_test.go
@@ -93,7 +93,7 @@ func TestIT_UserService_Get_WhenUserDoesNotExist_ExpectFailure(t *testing.T) {
 	_, err := service.Get(context.Background(), nonExistingId)
 
 	assert := assert.New(t)
-	assert.True(errors.IsErrorWithCode(err, db.NoMatchingRows))
+	assert.True(errors.IsErrorWithCode(err, db.NoMatchingRows), "Actual err: %v", err)
 }
 
 func TestIT_UserService_List(t *testing.T) {
@@ -194,9 +194,108 @@ func TestIT_UserService_Delete_WhenUserIsLoggedIn_ExpectApiKeyAlsoDeleted(t *tes
 	assertUserDoesNotExist(t, conn, user.Id)
 }
 
-// Login
-// LoginById
-// Logout
+func TestIT_UserService_Login_ExpectCorrectUserAndValidity(t *testing.T) {
+	service, conn := newTestUserRepository(t)
+	user := insertTestUser(t, conn)
+
+	userDtoRequest := communication.UserDtoRequest{
+		Email:    user.Email,
+		Password: user.Password,
+	}
+
+	apiKey, err := service.Login(context.Background(), userDtoRequest)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.Equal(user.Id, apiKey.User)
+	validityDateWithSafetyMargin := time.Now().Add(55 * time.Minute)
+	assert.True(apiKey.ValidUntil.After(validityDateWithSafetyMargin))
+	assertApiKeyExistsByKey(t, conn, apiKey.Key)
+}
+
+func TestIT_UserService_Login_WhenUserDoesNotExist_ExpectFailure(t *testing.T) {
+	userDtoRequest := communication.UserDtoRequest{
+		Email:    fmt.Sprintf("not-an-existing-email-%s", uuid.New()),
+		Password: "my-password",
+	}
+
+	service, _ := newTestUserRepository(t)
+	_, err := service.Login(context.Background(), userDtoRequest)
+
+	assert := assert.New(t)
+	assert.True(errors.IsErrorWithCode(err, db.NoMatchingRows), "Actual err: %v", err)
+}
+
+func TestIT_UserService_Login_WhenCredentialsAreWrong_ExpectFailure(t *testing.T) {
+	service, conn := newTestUserRepository(t)
+	user := insertTestUser(t, conn)
+
+	userDtoRequest := communication.UserDtoRequest{
+		Email:    user.Email,
+		Password: "not-the-right-password",
+	}
+
+	_, err := service.Login(context.Background(), userDtoRequest)
+
+	assert := assert.New(t)
+	assert.True(errors.IsErrorWithCode(err, InvalidCredentials), "Actual err: %v", err)
+}
+
+func TestIT_UserService_Login_WhenUserAlreadyLoggedIn_ExpectApiKeyValidityIsExtended(t *testing.T) {
+	service, conn := newTestUserRepository(t)
+	user := insertTestUser(t, conn)
+	timeInThePast := time.Now().Add(-1 * time.Hour)
+	apiKey := insertApiKeyForUserWithValidity(t, conn, user.Id, timeInThePast)
+
+	userDtoRequest := communication.UserDtoRequest{
+		Email:    user.Email,
+		Password: user.Password,
+	}
+
+	updatedApiKey, err := service.Login(context.Background(), userDtoRequest)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.Equal(apiKey.Key, updatedApiKey.Key)
+	assert.Equal(user.Id, updatedApiKey.User)
+	assert.True(timeInThePast.Before(updatedApiKey.ValidUntil))
+	validityDateWithSafetyMargin := time.Now().Add(55 * time.Minute)
+	assert.True(updatedApiKey.ValidUntil.After(validityDateWithSafetyMargin))
+}
+
+func TestIT_UserService_Logout(t *testing.T) {
+	service, conn := newTestUserRepository(t)
+	user := insertTestUser(t, conn)
+	apiKey := insertApiKeyForUser(t, conn, user.Id)
+
+	err := service.Logout(context.Background(), user.Id)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assertApiKeyDoesNotExist(t, conn, apiKey.Id)
+	assertUserExists(t, conn, user.Id)
+}
+
+func TestIT_UserService_Logout_WhenUserDoesNotExist_ExpectFailure(t *testing.T) {
+	nonExistingId := uuid.MustParse("00000000-0000-1221-0000-000000000000")
+
+	service, _ := newTestUserRepository(t)
+	err := service.Logout(context.Background(), nonExistingId)
+
+	assert := assert.New(t)
+	assert.True(errors.IsErrorWithCode(err, db.NoMatchingRows), "Actual err: %v", err)
+}
+
+func TestIT_UserService_Logout_WhenNotLoggedIn_ExpectSuccess(t *testing.T) {
+	service, conn := newTestUserRepository(t)
+	user := insertTestUser(t, conn)
+
+	err := service.Logout(context.Background(), user.Id)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assertUserExists(t, conn, user.Id)
+}
 
 func newTestUserRepository(t *testing.T) (UserService, db.Connection) {
 	conn := newTestConnection(t)

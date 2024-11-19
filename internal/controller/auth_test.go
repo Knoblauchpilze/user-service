d02f74379906d3439f666522da0cce402ccbad91
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-11-19 21:22:52 +0100

    feat: Made controller test helper functions generic

--- internal/controller/auth_test.go
+++ internal/controller/auth_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/KnoblauchPilze/user-service/pkg/communication"
 	"github.com/KnoblauchPilze/user-service/pkg/errors"
 	"github.com/google/uuid"
-	"github.com/stretchr/testify/require"
 )
 
 type mockAuthService struct {
@@ -25,7 +24,7 @@ func TestUnit_AuthController_WhenNoApiKeyProvided_ExpectBadRequest(t *testing.T)
 	m := &mockAuthService{}
 	expectedBody := []byte("\"Invalid API key\"\n")
 
-	assertStatusCodeAndBody(t, req, m, http.StatusBadRequest, expectedBody)
+	assertStatusCodeAndBody[service.AuthService](t, req, m, authUser, http.StatusBadRequest, expectedBody)
 }
 
 func TestUnit_AuthController_WhenMultipleApiKeysProvided_ExpectBadRequest(t *testing.T) {
@@ -36,7 +35,7 @@ func TestUnit_AuthController_WhenMultipleApiKeysProvided_ExpectBadRequest(t *tes
 	m := &mockAuthService{}
 	expectedBody := []byte("\"Invalid API key\"\n")
 
-	assertStatusCodeAndBody(t, req, m, http.StatusBadRequest, expectedBody)
+	assertStatusCodeAndBody[service.AuthService](t, req, m, authUser, http.StatusBadRequest, expectedBody)
 }
 
 func TestUnit_AuthController_WhenApiKeyHasWrongSyntax_ExpectBadRequest(t *testing.T) {
@@ -46,7 +45,7 @@ func TestUnit_AuthController_WhenApiKeyHasWrongSyntax_ExpectBadRequest(t *testin
 	m := &mockAuthService{}
 	expectedBody := []byte("\"Invalid API key\"\n")
 
-	assertStatusCodeAndBody(t, req, m, http.StatusBadRequest, expectedBody)
+	assertStatusCodeAndBody[service.AuthService](t, req, m, authUser, http.StatusBadRequest, expectedBody)
 }
 
 func TestUnit_AuthController_WhenUserNotAuthenticated_ExpectForbidden(t *testing.T) {
@@ -62,7 +61,7 @@ func TestUnit_AuthController_WhenUserNotAuthenticated_ExpectForbidden(t *testing
 		"Message": "An unexpected error occurred"
 	}`
 
-	assertStatusCodeAndJsonBody(t, req, m, http.StatusForbidden, expectedBody)
+	assertStatusCodeAndJsonBody[service.AuthService](t, req, m, authUser, http.StatusForbidden, expectedBody)
 }
 
 func TestUnit_AuthController_WhenApiKeyIsExpired_ExpectForbidden(t *testing.T) {
@@ -78,7 +77,7 @@ func TestUnit_AuthController_WhenApiKeyIsExpired_ExpectForbidden(t *testing.T) {
 		"Message": "An unexpected error occurred"
 	}`
 
-	assertStatusCodeAndJsonBody(t, req, m, http.StatusForbidden, expectedBody)
+	assertStatusCodeAndJsonBody[service.AuthService](t, req, m, authUser, http.StatusForbidden, expectedBody)
 }
 
 func TestUnit_AuthController(t *testing.T) {
@@ -87,38 +86,9 @@ func TestUnit_AuthController(t *testing.T) {
 
 	m := &mockAuthService{}
 
-	assertStatusCode(t, req, m, http.StatusNoContent)
+	assertStatusCode[service.AuthService](t, req, m, authUser, http.StatusNoContent)
 }
 
 func (m *mockAuthService) Authenticate(ctx context.Context, apiKey uuid.UUID) (communication.AuthorizationDtoResponse, error) {
 	return communication.AuthorizationDtoResponse{}, m.err
-}
-
-func assertStatusCode(t *testing.T, req *http.Request, service service.AuthService, expectedStatusCode int) {
-	ctx, rw := generateTestEchoContextFromRequest(req)
-
-	err := authUser(ctx, service)
-
-	require.Nil(t, err)
-	require.Equal(t, expectedStatusCode, rw.Code)
-}
-
-func assertStatusCodeAndBody(t *testing.T, req *http.Request, service service.AuthService, expectedStatusCode int, expectedBody []byte) {
-	ctx, rw := generateTestEchoContextFromRequest(req)
-
-	err := authUser(ctx, service)
-
-	require.Nil(t, err)
-	require.Equal(t, expectedStatusCode, rw.Code)
-	require.Equal(t, expectedBody, rw.Body.Bytes())
-}
-
-func assertStatusCodeAndJsonBody(t *testing.T, req *http.Request, service service.AuthService, expectedStatusCode int, expectedJsonBody string) {
-	ctx, rw := generateTestEchoContextFromRequest(req)
-
-	err := authUser(ctx, service)
-
-	require.Nil(t, err)
-	require.Equal(t, expectedStatusCode, rw.Code)
-	require.JSONEq(t, expectedJsonBody, rw.Body.String())
 }

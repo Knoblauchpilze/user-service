cfb6db2e1ffc1ff5c26ffa4e936555e0c820bb2f
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-11-19 21:35:27 +0100

    feat: Configurable logger for the server and refined main

--- cmd/users/main.go
+++ cmd/users/main.go
@@ -2,19 +2,56 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"os"
 	"time"
 
+	"github.com/KnoblauchPilze/user-service/internal/controller"
+	"github.com/KnoblauchPilze/user-service/internal/service"
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/KnoblauchPilze/user-service/pkg/db/postgresql"
+	"github.com/KnoblauchPilze/user-service/pkg/logger"
+	"github.com/KnoblauchPilze/user-service/pkg/repositories"
 	"github.com/KnoblauchPilze/user-service/pkg/server"
 )
 
 func main() {
+	log := logger.New(logger.NewPrettyWriter(os.Stdout))
+
+	dbName := "my-db"
+	dbUser := "my-user"
+	dbPassword := "my-password"
+	dbConfig := postgresql.NewConfigForLocalhost(dbName, dbUser, dbPassword)
+	conn, err := db.New(context.Background(), dbConfig)
+	if err != nil {
+		log.Errorf("Failed to create db connection: %v", err)
+		os.Exit(1)
+	}
+
+	repos := repositories.Repositories{
+		User: repositories.NewUserRepository(conn),
+	}
+
+	apiKeyConfig := service.ApiKeyConfig{
+		Validity: 5 * time.Minute,
+	}
+	userService := service.NewUserService(apiKeyConfig, conn, repos)
+
 	config := server.Config{
 		Port:            1234,
 		ShutdownTimeout: 2 * time.Second,
 	}
+	s := server.NewWithLogger(config, log)
 
-	s := server.New(config)
-	err := s.Start(context.Background())
-	fmt.Printf("err: %v\n", err)
+	for _, route := range controller.UserEndpoints(userService) {
+		if err := s.AddRoute(route); err != nil {
+			log.Errorf("Failed to register route %v: %v", route.Path(), err)
+			os.Exit(1)
+		}
+	}
+
+	err = s.Start(context.Background())
+	if err != nil {
+		log.Errorf("Error while serving: %v", err)
+		os.Exit(1)
+	}
 }

bc92b4fee37ba65d2f800264b580bfcbcdfaac31
KnoblauchPilze <prolific-programmer@tuta.com>
2024-11-20 21:07:20 +0100

    feat: Added docker container default and test for postgresql db config

--- pkg/db/postgresql/config_test.go
+++ pkg/db/postgresql/config_test.go
@@ -19,7 +19,24 @@ func TestUnit_Config_ExpectCorrectConnectionString(t *testing.T) {
 
 	actual := config.ToConnectionString()
 
-	assert := assert.New(t)
 	expected := "postgresql://super-user:strong-password%3F@my-host:8976/my-database?connect_timeout=7200"
-	assert.Equal(expected, actual)
+	assert.Equal(t, expected, actual)
+}
+
+func TestUnit_Config_ConfigForLocalhost(t *testing.T) {
+	config := NewConfigForLocalhost("my-local-database", "my-local-user", "my-local-password")
+
+	actual := config.ToConnectionString()
+
+	expected := "postgresql://my-local-user:my-local-password@localhost:5432/my-local-database?connect_timeout=5"
+	assert.Equal(t, expected, actual)
+}
+
+func TestUnit_Config_ConfigForDockerContainer(t *testing.T) {
+	config := NewConfigForDockerContainer("my-remote-database", "my-remote-user", "my-remote-password")
+
+	actual := config.ToConnectionString()
+
+	expected := "postgresql://my-remote-user:my-remote-password@172.17.0.1:5432/my-remote-database?connect_timeout=5"
+	assert.Equal(t, expected, actual)
 }

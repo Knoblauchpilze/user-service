0135e562d09501a82e1ef8035550ac296be2a796
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-11-20 20:53:53 +0100

    feat: Added a test to document not working behavior

--- pkg/config/config_loader_test.go
+++ pkg/config/config_loader_test.go
@@ -96,13 +96,34 @@ func TestUnit_Load_WhenEnvironmentVariableExists_ExpectTakesPrecedenceOverConfig
 	assert.Equal(t, uint16(26), actual.Server.Port)
 }
 
+func TestUnit_Load_WhenConfigDoesNotExistInFileButEnvironmentVariableDoes_ExpectValueNotOverridden(t *testing.T) {
+	// https://github.com/spf13/viper/issues/1797
+	// This might change with the next release
+	configName := writeConfigFile(t, nil)
+
+	in := sampleConfig{
+		Server: sampleServerConfig{
+			Port: 22,
+		},
+	}
+
+	t.Setenv("ENV_SERVER_PORT", "26")
+
+	actual, err := Load(configName, in)
+	assert.Nil(t, err)
+	assert.Equal(t, uint16(22), actual.Server.Port)
+}
+
 func writeSampleConfigFile(t *testing.T) string {
 	// https://stackoverflow.com/questions/19975954/a-yaml-file-cannot-contain-tabs-as-indentation
 	sampleYaml := "Server:\n  Port: 20\n"
+	return writeConfigFile(t, []byte(sampleYaml))
+}
 
+func writeConfigFile(t *testing.T, content []byte) string {
 	configName := fmt.Sprintf("config-%s", uuid.New())
 	configFileName := fmt.Sprintf("configs/%s.yml", configName)
-	err := os.WriteFile(configFileName, []byte(sampleYaml), 0666)
+	err := os.WriteFile(configFileName, content, 0666)
 	require.Nil(t, err)
 
 	return configName

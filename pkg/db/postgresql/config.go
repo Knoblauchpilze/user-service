fb67a48e0996c8dd53eedbfe1a1eb4ae56e69ee9
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-11-20 21:07:20 +0100

    feat: Added docker container default and test for postgresql db config

--- pkg/db/postgresql/config.go
+++ pkg/db/postgresql/config.go
@@ -29,3 +29,15 @@ func NewConfigForLocalhost(database string, user string, password string) Config
 		ConnectTimeout: defaultConnectTimeout,
 	}
 }
+
+func NewConfigForDockerContainer(database string, user string, password string) Config {
+	return Config{
+		// https://stackoverflow.com/questions/68173651/connecting-to-a-localhost-postgres-database-from-within-a-docker-container
+		Host:           "172.17.0.1",
+		Port:           5432,
+		Database:       database,
+		User:           user,
+		Password:       password,
+		ConnectTimeout: defaultConnectTimeout,
+	}
+}

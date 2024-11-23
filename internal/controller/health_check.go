4e5567aae3e250356fc00ba755c1de2c74edb33b
KnoblauchPilze <prolific-programmer@tuta.com>
2024-11-23 17:51:03 +0100

    feat: Added health check controller

---
+++ internal/controller/health_check.go
@@ -0,0 +1,28 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/KnoblauchPilze/user-service/pkg/rest"
+	"github.com/labstack/echo/v4"
+)
+
+func HealthCheckEndpoints(pool db.Connection) rest.Routes {
+	var out rest.Routes
+
+	getHandler := createServiceAwareHttpHandler(healthcheck, pool)
+	get := rest.NewRoute(http.MethodGet, "/healthcheck", getHandler)
+	out = append(out, get)
+
+	return out
+}
+
+func healthcheck(c echo.Context, pool db.Connection) error {
+	err := pool.Ping(c.Request().Context())
+	if err != nil {
+		return c.JSON(http.StatusServiceUnavailable, err)
+	}
+
+	return c.JSON(http.StatusOK, "OK")
+}
